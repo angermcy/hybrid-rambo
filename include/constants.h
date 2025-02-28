6ce3c72694ebd289837eb564e1476c35b848a0b2
angermcy <50660629+angermcy@users.noreply.github.com>
2025-02-28 07:31:03 +0100

    Create constants.h

---
+++ include/constants.h
@@ -0,0 +1,13 @@
+// constants.h
+
+#ifndef _MYLIB_CONSTANTS_H_
+#define _MYLIB_CONSTANTS_H_
+
+/*
+ *  define constants
+ */
+//const int Ki = 27947252; // number of sets
+const int Ki_old = 100; // number of sets do not use
+const int murmurmaxseed = 3; //accuracy of hash 7=0.01 -> collision avoidance?
+
+#endif
